0976fb9b802ccbcbb8b2d8231f2bba8166dab123
xannik <rasmus.lindy@cygni.se>
2023-10-23 15:22:53 +0200

    Add init function to attendees

--- firmware/turn-cygnicator/main.c
+++ firmware/turn-cygnicator/main.c
@@ -3,6 +3,30 @@
 #include <semphr.h>
 #include <task.h>
 
+#include "cygnicator_headlights.h"
+
+/* 
+    Pico init function that will initialize and configure all gpios used in this workshop 
+*/
+static void init_pico(void)
+{
+    stdio_init_all();
+
+    gpio_init_mask(gpio_output_pins_mask | gpio_input_pins_mask);
+    gpio_set_dir_out_masked(gpio_output_pins_mask);
+    gpio_set_dir_in_masked(gpio_input_pins_mask);
+
+    gpio_set_function(gpio_buzzer_map[BUZZER_FRONT], GPIO_FUNC_PWM);
+    uint8_t const slice_num = pwm_gpio_to_slice_num(gpio_buzzer_map[BUZZER_FRONT]);
+    pwm_set_enabled(slice_num, true);
+
+    gpio_pull_up(gpio_button_map[BUTTON_LEFT_INDICATOR]);
+    gpio_pull_up(gpio_button_map[BUTTON_RIGHT_INDICATOR]);
+    gpio_pull_up(gpio_button_map[BUTTON_BRAKE]);
+    gpio_pull_up(gpio_button_map[BUTTON_HAZARD]);
+
+}
+
 int main()
 {
     
